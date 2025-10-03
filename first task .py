42f477ba8adf7b8a2151aae6ec776d4a18dd63fb
amr-ya711 <aahyasserzz@gmail.com>
2025-10-04 00:37:40 +0300

    Add files via upload
    Create a Python script that calculates the total cost of items in a store and compares it with a given budget. The script should also calculate how much money is left or neededlo reach the budget.

---
+++ first task .py
@@ -0,0 +1,8 @@
+Total = float(input ("How much money do yo have ? \n"))
+x= float(input(" How much is the first item ? \n"))
+y= float(input(" How much is the second item ? \n"))
+z= float(input(" How much is the third item ? \n"))
+Cost= x + y + z 
+Rest = Total - Cost
+print(f" The Coast of All Item is : {Cost}" )
+print (f"The Rest Of Money You Have : {Rest}")  
