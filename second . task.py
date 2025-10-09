839173a8870b206b2d63405b29afed0ecc4bd4d6
amr-ya711 <aahyasserzz@gmail.com>
2025-10-09 15:45:26 +0300

    Add files via upload
    Session 3 task

---
+++ second . task.py
@@ -0,0 +1,19 @@
+# Task: Weather Advisor
+Temperature = float(input("Enter the current Temperature in Celsius: "))
+
+if Temperature >= 30:
+    print("It's a hot day. Stay hydrated!")
+elif Temperature >= 20 and Temperature <= 29:
+    print("It's a warm day. Enjoy the weather!")
+elif Temperature >= 10 and Temperature <= 19:
+    print("It's a cool day. Wear a jacket!")
+else:
+    print("It's a cold day. Stay warm!")
+
+# Task: Filter and Transform a List
+
+print("Numbers divisible by 3:")
+for Num in range(1, 21):
+    if Num % 3 == 0:
+        print(Num)
+
