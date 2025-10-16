f8fa08a6031e62d9f0f1597f1cca75be70a9c2a9
amr-ya711 <aahyasserzz@gmail.com>
2025-10-16 21:26:54 +0300

    third task 2.py

--- third task 2.py
+++ third task 2.py
@@ -15,6 +15,5 @@ def calculator(a, b, operation):
 a = float(input("Enter The First Number : "))
 b = float(input("Enter The Second Number :  "))
 operation = input("Enter The Operation : ").lower()
-
 result = calculator(a, b, operation)
 print(f"The Result :{result}")    
