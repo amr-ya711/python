253589cafe87795ecccce29933d1b73328301390
amr-ya711 <aahyasserzz@gmail.com>
2025-10-16 21:25:33 +0300

    Add files via upload
    Custom Calculator

---
+++ third task 2.py
@@ -0,0 +1,20 @@
+def calculator(a, b, operation):
+    if operation == "+":
+        return a + b
+    elif operation == "-":
+        return a - b
+    elif operation == "*":
+        return a * b
+    elif operation == "/":
+        if b == 0:
+            return "Error  Cannot divide by zero"
+        return a / b
+    else:
+        return "Invalid operation"
+    
+a = float(input("Enter The First Number : "))
+b = float(input("Enter The Second Number :  "))
+operation = input("Enter The Operation : ").lower()
+
+result = calculator(a, b, operation)
+print(f"The Result :{result}")    
