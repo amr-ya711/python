38519a8538a684254fdab0668a17c289d5e8534f
amr-ya711 <aahyasserzz@gmail.com>
2025-10-23 19:41:15 +0300

    book.py

--- oop python /book.py
+++ oop python /book.py
@@ -5,3 +5,12 @@ class Book:
         self.__isbn = isbn   
         available = True     
         self.available = available
+
+    def get_isbn(self):
+        return self.__isbn
+    def set_isbn(self, new_isbn):
+        self.__isbn = new_isbn
+
+    def display_info(self):
+        status = "Available" if self.available else "Not Available"
+        print(f"Title: {self.title}, Author: {self.author}, ISBN: {self.__isbn}, Status: {status}")
